3da9af324f9f5dbc265b639e681d5bb2d52ef06a
assasinooz77 <assasinooz77@gmail.com>
2024-06-20 21:57:17 +0800

    feat: update file names

--- src/app/(pages)/migrate/page.tsx
+++ src/app/(pages)/migrate/page.tsx
@@ -1,13 +1,11 @@
-import NavBar from "@/app/shared-components/NavBar";
+import NavBar from "@/app/shared/NavBar";
 import { SimpleGrid } from "@mantine/core";
 
-
 export default async function Migrate() {
-
-    return (
-        <>
-            <NavBar />
-            <h1 className="text-white">Migrate</h1>
-        </>
-    )
+  return (
+    <>
+      <NavBar />
+      <h1 className="text-white">Migrate</h1>
+    </>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,24 @@
 // 'use client';
-import NavBar from '@/app/shared-components/NavBar';
-import { Montserrat } from 'next/font/google'
-import { Account } from '../components/Account'
-import { WagmiProvider, useAccount } from 'wagmi'
-const montserrat = Montserrat({ subsets: ['latin'] })
-import './globals.css';
-import UniswapPools from '@/components/UniswapPools';
-import CurvePools from '@/components/CurvePools';
-import Farms from './(pages)/farms/page';
-import Migrate from './(pages)/migrate/page';
-
+import NavBar from "@/app/shared/NavBar";
+import { Montserrat } from "next/font/google";
+import { Account } from "../components/Account";
+import { WagmiProvider, useAccount } from "wagmi";
+const montserrat = Montserrat({ subsets: ["latin"] });
+import "./globals.css";
+import UniswapPools from "@/components/UniswapPools";
+import CurvePools from "@/components/CurvePools";
+import Farms from "./(pages)/farms/page";
+import Migrate from "./(pages)/migrate/page";
 
 export default function Home() {
-    return (
-        <main className={montserrat.className}>
-            <Migrate />
-        </main>
-
-    );
+  return (
+    <main className={montserrat.className}>
+      <Migrate />
+    </main>
+  );
 }
 
-
 function ConnectWallet() {
-    const { isConnected } = useAccount()
-    if (isConnected) return [<Account />]
+  const { isConnected } = useAccount();
+  if (isConnected) return [<Account />];
 }
